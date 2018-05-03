1e747134dc619c3e8c2484638273cbc09759feb6
James Sudhir <jamessudhir@Jamess-Sudhir.local>
2018-05-03 10:05:20 +0530

    js added

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,9 @@
 $("#overlay").show();
 
+$(".gallery-grid").hide();
+
 setTimeout(function() {
   $("#overlay").fadeOut();
-  $("body").show(300);
+  $("body").show(500);
+  $(".gallery-grid").show(500);
 }, 10000);
